4908e8ae40dd76fa4973532e787b475c5c91e288
Louis <Louis.Medina@flatironschool.com>
2022-11-23 14:45:37 -0500

    add p1 notes for 03 lecture

--- 03_information_flow/project_showcase/src/App.js
+++ 03_information_flow/project_showcase/src/App.js
@@ -5,11 +5,34 @@ import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
 
 const App = () => {
+  
+  // Lifted isDarkMode State From Header to App
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  // Set Up a Callback Function Responsible for Updating 
+  // Our State
+  const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
+  // Storing Our Data in State Enables Us
+  // to Work with that Data In a More Flexible Way
   const [projects, setProjects] = useState([]);
 
   // # Deliverable 1: Configure a <button> in our App 
   // that will use json-server to fetch projects 
   // and store them in state
+
+  const fetchProjects = () => {
+    // Specify Request URL (Endpoint)
+    fetch('http://localhost:4000/projects')
+      
+      // Parsing Out the JSON Response into JS
+      .then(res => res.json())
+      
+      // Set JS Data as New Value (Array) for Projects
+      .then(data => {
+        setProjects(data);
+      });
+  }
 
   // - Add an onClick event listener to the "Load Projects" 
   // button
@@ -20,10 +43,13 @@ const App = () => {
   // returned by the response
 
   return (
-    <div className="App">
-      <Header />
+    <div className={ isDarkMode ? "App" : "App light"}>
+      <Header 
+        isDarkMode={isDarkMode} 
+        onToggleDarkMode={onToggleDarkMode} 
+      />
       <ProjectForm />
-      <button>Load Projects</button>
+      <button onClick={fetchProjects}>Load Projects</button>
       <ProjectList projects={projects} />
     </div>
   );
