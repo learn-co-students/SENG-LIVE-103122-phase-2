324c3167c772ecf8581d98f9b9abb2aeff42a508
Louis <Louis.Medina@flatironschool.com>
2022-11-29 15:32:11 -0500

    add p3 notes for 05 lecture

--- 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
@@ -13,24 +13,40 @@ const Timer = () => {
     
     const [count, setCount] = useState(0);
 
+    const logTimer = () => {
+        console.log("Page Clicked!");
+    }
+
     useEffect(() => {
-        let intervalId = setInterval(() => {
-            setCount(prevCount => prevCount + 1);           
-            
-            console.log("Timer Firing");
-            // console.log(intervalId);
-        }, 1000);
+        
+        // Side Effect Behavior
 
-        // Clean Up Behavior
+            let intervalId = setInterval(() => {
+                // setCount(count + 1);
+                
+                setCount(prevCount => prevCount + 1);           
+                
+                console.log("SIDE EFFECT");
+                // console.log(intervalId);
+            }, 1000);
 
-        return () => {
+            document.addEventListener("click", logTimer);
 
+        const clearEverything = () => {
             // Add Syntax to "Clean Up" setInterval
             clearInterval(intervalId);
 
+            // Add Syntax to "Clean Up" (Remove) Event Listener
+            document.removeEventListener("click", logTimer);
+        }
+
+        // Clean Up Behavior
+        return () => {
+            console.log("CLEAN UP");
+            clearEverything();
         }
         
-    }, [])
+    }, []);
 
     return (
         <h1>Timer Count: {count}</h1>
